a4c1103c2e881ae8c1583430ecddb8c3d066d7b9
Ryan <ryan@ispeakcomputer.com>
2020-05-09 02:53:35 -0600

    working now

--- comment_bomb.py
+++ comment_bomb.py
@@ -7,21 +7,24 @@ import time
 
 selenium = webdriver.Chrome()
 
-driver.get("http://mywebsite/about-2/")
+#driver = webdriver.Chrome('chromedriver')  # Optioznal argument, if not specified will search path.
+selenium.get("https://www.page.com/contact")
 count = 0 
-while count < 666666666666666666666666666666666666666666666:
+while count < 2:
     count = count + 1
-    print count
+    print(count)
 
 
-    comment = selenium.find_element_by_id("comment")
-    author = selenium.find_element_by_id("author")
-    email = selenium.find_element_by_id("email")
+    comment = selenium.find_element_by_id("ContactForm-message")
+    name = selenium.find_element_by_id("ContactForm-name")
+    phone = selenium.find_element_by_id("ContactForm-phone")
+    email = selenium.find_element_by_id("ContactForm-email")
      
-    comment.send_keys("this is message")
-    email.send_keys(this@test.com")
-    author.send_keys("señor. comentarista")
+    comment.send_keys("I would like to order")
+    email.send_keys("ted@gmail.com")
+    phone.send_keys("796-777-6979")
+    name.send_keys("ted")
      
-    selenium.find_element_by_name("submit").click()
-    print("party time number" + "  " + count)
-    time.sleep(5)
+    selenium.find_element_by_css_selector('input[type="submit"]').submit()
+    print("party time number" + "  " + str(count))
+    time.sleep(15)
